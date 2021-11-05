b707da6e9b6467bcd9a4e3beb5f6e400ea5fa29c
Michael <michaeljohntupas@gmail.com>
2021-11-06 00:53:42 +0800

    first commit

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit, Injectable } from '@angular/core';
-import {CdkDragDrop, moveItemInArray, transferArrayItem} from '@angular/cdk/drag-drop';
+import { CdkDragDrop, moveItemInArray, transferArrayItem } from '@angular/cdk/drag-drop';
 import { AuthServiceService } from '../auth-service.service';
 import { Router } from '@angular/router';
 import { JwtHelperService } from "@auth0/angular-jwt";
@@ -15,24 +15,24 @@ import { Message } from '@angular/compiler/src/i18n/i18n_ast';
 })
 export class HomeComponent implements OnInit {
   helper = new JwtHelperService();
-  userid:any;
-  username:any;
+  userid: any;
+  username: any;
   // todo = ['Get to work', 'Pick up groceries', 'Go home', 'Fall asleep'];
   // ongo = ['Get to work', 'Pick up groceries', 'Go home', 'Fall asleep'];
   // done = ['Get up', 'Brush teeth', 'Take a shower', 'Check e-mail', 'Walk dog'];
-  
+
   todos = [
-    { id:0, taskTitle: '', taskDesc: '', sorting: 0, status: 0},
+    { id: 0, taskTitle: '', taskDesc: '', sorting: 0, status: 0 },
   ];
   completed = [
-    { id:0, taskTitle: '', taskDesc: '', sorting: 0, status: 0},
+    { id: 0, taskTitle: '', taskDesc: '', sorting: 0, status: 0 },
   ];
 
-  
-  constructor(private authService:AuthServiceService, private router:Router, public dialog: MatDialog ) { }
+
+  constructor(private authService: AuthServiceService, private router: Router, public dialog: MatDialog) { }
 
   ngOnInit(): void {
-    if(this.checkToken()){
+    if (this.checkToken()) {
       alert("Token has been expired!");
       this.router.navigate(['login']);
     }
@@ -44,28 +44,33 @@ export class HomeComponent implements OnInit {
   }
   drop(event: CdkDragDrop<any[]>) {
     if (event.previousContainer === event.container) {
-      moveItemInArray(event.container.data, event.previousIndex, event.currentIndex);
-      
-      this.updateRequest(event.container.data);
-      this.getDataDone();
-      this.getDataTodo();
+
+      moveItemInArray(
+        event.container.data,
+        event.previousIndex,
+        event.currentIndex
+      );
+      this.updateRequestTodo();
+      this.updateRequestDone();
+
     } else {
-      
+
       transferArrayItem(
         event.previousContainer.data,
         event.container.data,
         event.previousIndex,
         event.currentIndex,
       );
-      this.updateRequest_status(event.container.data);
+      this.updateRequestTodo();
+      this.updateRequestDone();
     }
   }
 
-  getDataTodo(){
-    this.authService.getDataTodo(this.userid).subscribe(result =>{
-      if(result.success){
-        let sorting=0;
-        this.todos = result.data.map((item: { id: any, taskTitle: any, taskDesc:any, sorting:any, status:any}) =>{
+  getDataTodo() {
+    this.authService.getDataTodo(this.userid).subscribe(result => {
+      if (result.success) {
+        let sorting = 0;
+        this.todos = result.data.map((item: { id: any, taskTitle: any, taskDesc: any, sorting: any, status: any }) => {
           return {
             id: item.id,
             taskTitle: item.taskTitle,
@@ -74,19 +79,19 @@ export class HomeComponent implements OnInit {
             status: item.status
           };
         });
-      }else{
-        if(result.message == "Record not found!"){
+      } else {
+        if (result.message == "Record not found!") {
           this.todos = [];
         }
       }
-    }); 
+    });
   }
 
-  getDataDone(){
-    this.authService.getDataDone(this.userid).subscribe(result =>{
-      if(result.success){
-        let sorting=0;
-        this.completed = result.data.map((item: { id: any, taskTitle: any, taskDesc:any, sorting:any, status:any}) =>{
+  getDataDone() {
+    this.authService.getDataDone(this.userid).subscribe(result => {
+      if (result.success) {
+        let sorting = 0;
+        this.completed = result.data.map((item: { id: any, taskTitle: any, taskDesc: any, sorting: any, status: any }) => {
           return {
             id: item.id,
             taskTitle: item.taskTitle,
@@ -95,61 +100,58 @@ export class HomeComponent implements OnInit {
             status: item.status
           };
         });
-      }else{
-        if(result.message == "Record not found!"){
+      } else {
+        if (result.message == "Record not found!") {
           this.completed = [];
         }
       }
-    }); 
+    });
   }
-  updateRequest(data:any){
+  updateRequestTodo() {
+    let status = 0;
     let sorting = 0;
-    for(var item of data){
-      item.sorting = sorting++;
-      this.updateTaskProcess(item.id, item);
-    }
-  }
-
-  updateRequest_status(data:any){
-    let status=0;
-    let sorting = 0;
-    for(var item of data){
-      if(item.status=1){
-        status=0;
-      }else{
-        status=1;
-      }
+    for (var item of this.todos) {
       item.sorting = sorting++;
       item.status = status;
       this.updateTaskProcess(item.id, item);
     }
   }
 
-  updateTaskProcess(id:any, data:any){
-    this.authService.updateTask(id, data).subscribe(result =>{});
+  updateRequestDone() {
+    let status = 1;
+    let sorting = 0;
+    for (var item of this.completed) {
+      item.sorting = sorting++;
+      item.status = status;
+      this.updateTaskProcess(item.id, item);
+    }
   }
 
-  deleteTask(id:any){
-    this.authService.deleteTask(id).subscribe(result =>{  
-      if(result.success){
+  updateTaskProcess(id: any, data: any) {
+    this.authService.updateTask(id, data).subscribe(result => { });
+  }
+
+  deleteTask(id: any) {
+    this.authService.deleteTask(id).subscribe(result => {
+      if (result.success) {
         alert(result.message);
         window.location.reload();
-      }else{
+      } else {
         alert(result.message);
       }
     });
   }
 
-  addTask(){
+  addTask() {
     this.dialog.open(AddTaskComponent);
   }
 
-  logoutUser(){
+  logoutUser() {
     localStorage.removeItem('token');
-    this.router.navigate(['login']);    
+    this.router.navigate(['login']);
   }
 
-  checkToken(){
+  checkToken() {
     var token = localStorage.getItem('token');
     return this.helper.isTokenExpired(token?.toString());
   }
